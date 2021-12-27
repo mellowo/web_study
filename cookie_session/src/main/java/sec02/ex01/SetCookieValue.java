796a3219d9171bcc11181ac98e946988507ac924
lgh_0326 <lgh_0326@10.192.193.95>
2021-12-27 16:03:59 +0900

    commit

---
+++ cookie_session/src/main/java/sec02/ex01/SetCookieValue.java
@@ -0,0 +1,39 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SetCookieValue
+ */
+@WebServlet("/set")
+public class SetCookieValue extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Cookie c = new Cookie("cookeTest", URLEncoder.encode("JSP프로그래밍입니다.","UTF-8"));
+		c.setMaxAge(24*60*60);
+		response.addCookie(c);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
