b8e7bb61cb7ef8d55c2a56ab81b139e9f570a492
lgh_0326 <lgh_0326@10.192.193.95>
2022-02-03 17:33:18 +0900

    mvc study

---
+++ mvc/src/main/java/sec01/ex01/MemberController.java
@@ -0,0 +1,56 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberController
+ */
+@WebServlet("/mem.do")
+public class MemberController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	MemberDAO memberDAO;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		memberDAO = new MemberDAO();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		List<MemberVO> memberList = memberDAO.listMembers();
+		request.setAttribute("memberList", memberList);
+		System.out.println(memberList.get(0).getId());
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher("test01/listMembers.jsp");
+		dispatch.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ mvc/src/main/java/sec01/ex01/MemberVO.java
@@ -0,0 +1,54 @@
+package sec01.ex01;
+
+public class MemberVO {
+	private String id; 
+	private String pwd;
+	private String name ;
+	private String email ; 
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setIdx(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public MemberVO() {
+		super();
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+}
